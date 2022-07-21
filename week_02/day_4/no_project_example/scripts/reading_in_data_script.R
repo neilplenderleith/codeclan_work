73401aa965dba2efbacb1d44642feb11607e03ca
neilplenderleith <106347514+neilplenderleith@users.noreply.github.com>
2022-07-21 13:53:01 +0100

    mid-day commit of notes

---
+++ week_02/day_4/no_project_example/scripts/reading_in_data_script.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+
+#check what your working directory is
+getwd()
+
+#using tab navigate to the students.csv which is saved in the 'data' folder in the same folder as this .R file
+read_csv("CODECLAN/codeclan_work/week_02/day_4/no_project_example/data/students.csv")
---
+++ week_02/day_4/project_example/scripts/reading_in_data_script.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+
+#check what your working directory is
+getwd()
+#you should see it's the root directory of the project (i.e. wherever the .Rproj file sits)
+
+#using tab navigate to the students.csv which is saved in the 'data' folder in the same folder as this .R file
+read_csv("")
+#you will see this filepath is relative to the project. 
