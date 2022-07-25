b4a4766ba2c7110dbe73677522dc60ce1429936b
neilplenderleith <106347514+neilplenderleith@users.noreply.github.com>
2022-07-25 11:27:03 +0100

    Create first_queries.sql

---
+++ week_03/day_1/first_queries.sql
@@ -0,0 +1,34 @@
+/*
+* This is a multiline comment
+*/
+-- This is an inline comment
+
+
+
+-- Get me a table of all the animals information
+
+SELECT *
+FROM animals;
+
+-- READ operation
+
+
+
+
+-- Get me a table of information about the nimal with id=2
+
+SELECT *
+FROM animals
+WHERE id = 2;
+
+
+--  Task get em a table of information about ernest the snake
+
+SELECT *
+FROM animals
+WHERE name = 'Ernest' AND species = 'snake';
+
+-- because we are filtering on a primary key we should only expect 1 row
+SELECT *
+FROM animals
+WHERE id = 7;
